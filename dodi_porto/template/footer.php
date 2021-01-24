d0488df842453a2a037a63233af227e2c95ea107
dodinovembri <dodinovembri32@gmail.com>
2021-01-24 15:39:57 +0700

    first commit

---
+++ dodi_porto/template/footer.php
@@ -0,0 +1,55 @@
+
+		<!-- Vendor -->
+		<script src="../../assets/porto/vendor/jquery/jquery.min.js"></script>
+		<script src="../../assets/porto/vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="../../assets/porto/vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="../../assets/porto/vendor/jquery.cookie/jquery.cookie.min.js"></script>
+		<script src="../../assets/porto/vendor/popper/umd/popper.min.js"></script>
+		<script src="../../assets/porto/vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="../../assets/porto/vendor/common/common.min.js"></script>
+		<script src="../../assets/porto/vendor/jquery.validation/jquery.validate.min.js"></script>
+		<script src="../../assets/porto/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+		<script src="../../assets/porto/vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="../../assets/porto/vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+		<script src="../../assets/porto/vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="../../assets/porto/vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="../../assets/porto/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="../../assets/porto/vendor/vide/jquery.vide.min.js"></script>
+		<script src="../../assets/porto/vendor/vivus/vivus.min.js"></script>
+		<script src="../../assets/porto/vendor/jquery.countdown/jquery.countdown.min.js"></script>
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src="../../assets/porto/js/theme.js"></script>
+		
+		<!-- Current Page Vendor and Views -->
+		<script src="../../assets/porto/vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+		<script src="../../assets/porto/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="../../assets/porto/js/views/view.contact.js"></script>
+		
+		<!-- Demo -->
+		<script src="../../assets/porto/js/demos/demo-wedding.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src="../../assets/porto/js/custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src="../../assets/porto/js/theme.init.js"></script>
+		
+
+		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+			ga('create', 'UA-12345678-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+		 -->
+
+
+	</body>
+</html>
